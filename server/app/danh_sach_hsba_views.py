d6c675f6ecb6fa64e35f2acc5ed0c1fa5f9e99fa
anhnguyen6529 <anhnguyen6529@gmail.com>
2022-05-23 03:46:43 +0700

    Fix: Content creating of user's notifications

--- server/app/danh_sach_hsba_views.py
+++ server/app/danh_sach_hsba_views.py
@@ -102,7 +102,7 @@ def createNewHSBA():
     conn.commit()
 
     cursor.execute("INSERT INTO NOTIFICATIONS (User_ID, Type, Content, Status, Time_Created, Time_Seen) VALUES (\'" +
-                   data["benhAn"]["bacSiLamBenhAn"]["id"] + "', 'Created', JSON_OBJECT('hoTen', '" + data["hanhChinh"]["hoTen"] + "', 'pid', '" + data["pid"] + "'), 'Chưa đọc', '" + data["lyDoVaoVien"]["ngayVaoVien"] + "', '');")
+                   data["benhAn"]["bacSiLamBenhAn"]["id"] + "', 'Created', JSON_OBJECT('name', '" + data["hanhChinh"]["hoTen"] + "', 'pid', '" + data["pid"] + "'), 'Chưa đọc', '" + data["lyDoVaoVien"]["ngayVaoVien"] + "', '');")
     conn.commit()
 
     response = jsonify({"result": True})
